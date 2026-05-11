85754e2cc0ce777ffe98b9ccf4a399c564222469
sfelicetti <96916619+sfelicetti@users.noreply.github.com>
2026-05-11 09:56:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Parser = require("rss-parser");
-const cors = require("cors");
+const cors = require("cors"); 
 
 const app = express();
 const parser = new Parser();
